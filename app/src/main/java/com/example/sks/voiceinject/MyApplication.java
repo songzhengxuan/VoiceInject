9a13590872a6cdb6b7b463ec829307c421ea80bb
songzhengxuan <songzhengxuan@gmail.com>
2016-08-05 08:33:54 +0200

    reflect to get instrumentatino

--- app/src/main/java/com/example/sks/voiceinject/MyApplication.java
+++ app/src/main/java/com/example/sks/voiceinject/MyApplication.java
@@ -1,7 +1,11 @@
 package com.example.sks.voiceinject;
 
 import android.app.Application;
+import android.app.Instrumentation;
 import android.util.Log;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Created by sks on 2016/7/25.
@@ -13,5 +17,26 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "this app is " + this);
+        try {
+            Class<?> activityThreadClz = getClassLoader().loadClass("android.app.ActivityThread");
+            Log.d(TAG, "at class is " + activityThreadClz);
+            Method getMethod = activityThreadClz.getMethod("currentActivityThread", null);
+            Log.d(TAG, "method is " + getMethod);
+            getMethod.setAccessible(true);
+            Object atThreadObj = getMethod.invoke(null, null);
+            Log.d(TAG, "activityThread obj is " + atThreadObj);
+            Method getInstrumentationMethod = activityThreadClz.getMethod("getInstrumentation", null);
+            Log.d(TAG, "getInstrumentationMethod is " + getInstrumentationMethod);
+            Instrumentation instrumentation = (Instrumentation) getInstrumentationMethod.invoke(atThreadObj, null);
+            Log.d(TAG, "instrumentation is " + instrumentation);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
